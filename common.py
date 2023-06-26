b4cf4b5ecb64f79be06ff335c0eed5d5e124380f
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-06-27 02:18:46 +0900

    data

--- common.py
+++ common.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 @st.cache_data
 def get_sales():
-    return pd.read_csv("./Case.csv")
+    data = pd.read_csv("./Case.csv")
+    data[['latitude', 'longitude']] = data[['latitude', 'longitude']].replace("-", np.NaN)
+    data[['latitude', 'longitude']] = data[['latitude', 'longitude']].astype('f')
+    data.dropna(inplace=True)
+    return data
 
 def page_config():
     st.set_page_config(
