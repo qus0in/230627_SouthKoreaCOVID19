acb4515da9462351231b75bd7cf2297f5a0547a2
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-06-27 02:20:38 +0900

    Bubble Map Visualization

--- pages/03_Bubble Map Visualization.py
+++ pages/03_Bubble Map Visualization.py
@@ -5,7 +5,7 @@ import common
 
 common.page_config()
 
-st.title("Dot Map Visualization")
+st.title("Bubble Map Visualization")
 
 data = common.get_sales()
 
@@ -15,15 +15,17 @@ map_korea = folium.Map(location=[36.5, 127.5], zoom_start=7)
 # 데이터 시각화
 for idx, row in data.iterrows():
     lat, lon = row['latitude'], row['longitude']
+    confirmed_cases = row['confirmed']
     
-    # CircleMarker를 사용하여 도트맵 표현
+    # CircleMarker를 사용하여 버블맵 표현
     folium.CircleMarker(
         location=[lat, lon],
-        radius=1,
+        radius=confirmed_cases/1000,
         color='red',
         fill=True,
         fill_color='red',
-        fill_opacity=0.6
+        fill_opacity=0.6,
+        tooltip=f"Confirmed Cases: {confirmed_cases}"
     ).add_to(map_korea)
 
 # https://github.com/randyzwitch/streamlit-folium/blob/master/examples/streamlit_app.py
